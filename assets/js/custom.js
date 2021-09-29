ab635aeed6be11d1f3fe7427d2c9e5d1efaaedd2
Marjana Swarnaly <marjanaswarnaly@gmail.com>
2021-09-29 23:25:50 +0600

    initial push

---
+++ assets/js/custom.js
@@ -0,0 +1,7 @@
+//LIGHTBOX CUSTOM
+const lightbox = GLightbox({
+  'href': 'https://youtu.be/WPqXP_kLzpo',
+  'type': 'video',
+  'source': 'youtube', //vimeo, youtube or local
+  'width': 900,
+});
